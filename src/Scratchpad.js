943be6ccfaa3fd01ec6857c5e235db653fde422c
amick12 <amckee26@gmail.com>
2020-03-19 14:30:34 -0400

    Initial Commit

---
+++ src/Scratchpad.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// function Scratchpad() {
+//     return (
+//         <h1>Hello Scratchpad</h1>
+//     )
+
+// }
+
+
+
+
+// export default Scratchpad
